b9e7bb8a8276a51cde58e8b82a73aa8acd6102be
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-02-27 12:25:34 +0100

    confgi redis mzl stripe w nodemailer w firebasse push notification

--- src/shared/config/redis.ts
+++ src/shared/config/redis.ts
@@ -1,4 +1,9 @@
 import redis from 'redis'
+import { createClient } from "redis";
+
+const client = createClient({ url: process.env.REDIS_URL });
+client.connect().catch(console.error);
+
 
 
 const pubClient = redis.createClient()
@@ -7,4 +12,4 @@ pubClient.on("error",(err)=> console.error(err.message))
 subClient.on("error",(err)=> console.error(err.message))
 export async function subscribe(){}
 export async function publish(){}
-export {pubClient,subClient}
+export {pubClient,subClient,client}
