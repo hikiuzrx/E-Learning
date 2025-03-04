f829453103646ebf8de2836de44fe265ed8eeb78
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-03-04 19:56:22 +0100

    fix

--- src/interface/repository/IuserRepository.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "../../domain/User";
-
-export interface Iuser {
-    save(user: User): Promise<User>;
-    findByEmail(email: string): Promise<User | null>;
-    findById(id: string): Promise<User | null>;
-}
